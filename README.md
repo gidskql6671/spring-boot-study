fb477f68d45f4de22059d9076781b82d1738a31b
gidskql6671 <gidskql6671@gmail.com>
2021-06-22 22:36:08 +0900

    init commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# spring boot study
+
+Spring Boot 공부를 위한 프로젝트.
+빌드 툴로는 Maven을 사용한다.
---
+++ src/test/java/com/study/dong/DongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.dong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
