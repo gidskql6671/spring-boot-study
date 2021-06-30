adf03c0ed79e5eeb757c6305bb0821576dfc6f19
gidskql6671 <gidskql6671@gmail.com>
2021-06-30 19:14:03 +0900

    TestService의 객체를 생성자에서 주입하도록 함

--- src/main/java/com/study/dong/test/controller/TestController.java
+++ src/main/java/com/study/dong/test/controller/TestController.java
@@ -1,7 +1,7 @@
 package com.study.dong.test.controller;
 
 import com.study.dong.test.service.TestService;
-import com.study.dong.test.vo.TestVO;
+import com.study.dong.test.vo.TestMybatisVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,22 +9,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 @RequestMapping("/test")
 @Slf4j
 public class TestController {
+    private TestService testService;
+
 
     @Autowired
-    TestService testService;
+    public TestController(TestService testService) {
+        this.testService = testService;
+    }
+
 
     @GetMapping()
     public ModelAndView home(){
         ModelAndView mav = new ModelAndView("test");
 
-        List<TestVO> testList = testService.selectTest();
+        List<TestMybatisVO> testList = testService.selectTest();
         mav.addObject("list", testList);
 
         log.debug("debug 로그");
