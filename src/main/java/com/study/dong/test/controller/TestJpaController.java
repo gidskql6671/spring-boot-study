6e61eba515c375b083fec0d9f811e5677982e044
gidskql6671 <gidskql6671@gmail.com>
2021-07-04 18:18:50 +0900

    Mybatis 제거

--- src/main/java/com/study/dong/test/controller/TestJpaController.java
+++ src/main/java/com/study/dong/test/controller/TestJpaController.java
@@ -1,17 +1,12 @@
 package com.study.dong.test.controller;
 
 import com.study.dong.test.service.TestJpaService;
-import com.study.dong.test.service.TestService;
-import com.study.dong.test.vo.TestJpaVO;
-import com.study.dong.test.vo.TestMybatisVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-
-import java.util.List;
 
 
 @Controller
