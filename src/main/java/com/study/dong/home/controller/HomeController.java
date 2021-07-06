c67e961d41f768bea2778ce5e7cf1ab287a839dc
gidskql6671 <gidskql6671@gmail.com>
2021-07-06 13:29:12 +0900

    기본 homepage 뷰 제작

---
+++ src/main/java/com/study/dong/home/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.study.dong.home.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String getHomepage(Model model){
+
+        return "index";
+    }
+}
