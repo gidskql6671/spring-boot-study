50ea08e9f0c6406932fa0c13e7baf24efdf51f4c
gidskql6671 <gidskql6671@gmail.com>
2021-07-09 19:01:10 +0900

    패키지명 변경

--- src/main/java/com/study/dong/user/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.study.dong.user.repository;
-
-import com.study.dong.user.vo.UserVO;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<UserVO, Long> {
-
-}
