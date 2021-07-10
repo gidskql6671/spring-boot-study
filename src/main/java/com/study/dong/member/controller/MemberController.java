1639e6053cba0fb7eac1df4a92fe88def9f998d1
gidskql6671 <gidskql6671@gmail.com>
2021-07-10 20:52:06 +0900

    문제가 발생하는 mapping 수정

--- src/main/java/com/study/dong/member/controller/MemberController.java
+++ src/main/java/com/study/dong/member/controller/MemberController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class MemberController {
     private final MemberService memberService;
 
-    @GetMapping("/")
+    @GetMapping()
     public String index(){
         return "/member/index";
     }
