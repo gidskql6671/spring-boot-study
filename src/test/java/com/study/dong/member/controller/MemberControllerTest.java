cdf4d8abb5c0abc30faa3647e5bf5f69909d1ee4
gidskql6671 <gidskql6671@gmail.com>
2021-07-11 16:44:53 +0900

    회원가입 페이지 get요청에 대한 테스트 작성 완료

--- src/test/java/com/study/dong/member/controller/MemberControllerTest.java
+++ src/test/java/com/study/dong/member/controller/MemberControllerTest.java
@@ -1,6 +1,12 @@
 package com.study.dong.member.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.study.dong.member.dto.MemberDto;
 import com.study.dong.member.service.MemberService;
+import org.hamcrest.Matchers;
+import org.hamcrest.core.Is;
+import org.hamcrest.core.IsEqual;
+import org.hamcrest.object.HasEqualValues;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,18 +14,22 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.mockito.Mockito.when;
-
+import static org.hamcrest.core.Is.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(controllers = MemberController.class)
 class MemberControllerTest {
     @Autowired
     private MockMvc mockMvc;
-
+    
     @MockBean
     private MemberService memberService;
 
@@ -33,12 +43,27 @@ class MemberControllerTest {
         mockMvc.perform(
                 MockMvcRequestBuilders.get("/member")
                         .accept(MediaType.TEXT_HTML))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andDo(MockMvcResultHandlers.print())
+                .andExpect(status().isOk())
+                .andDo(print())
                 .andExpect(MockMvcResultMatchers.view().name("/member/index"));
     }
 
     @Test
     public void getSignupForm() throws Exception {
+        MemberDto memberDto = new MemberDto();
+        // samePropertyValuesAs를 테스트해보기위해...
+        String[] ignoreProperty = {"name", "nickname"};
+        memberDto.setName("ASD");
+        memberDto.setNickname("DDD");
+        
+        
+        mockMvc.perform(
+                    get("/member/signup")
+                        .accept(MediaType.TEXT_HTML))
+                // member 속성값의 클래스가 MemberDto인지 확인
+                .andExpect(model().attribute("member", Matchers.instanceOf(MemberDto.class)))
+                // member 속성값의 객체가 memberDto와 동일한 값을 가지는지 확인
+                .andExpect(model().attribute("member", Matchers.samePropertyValuesAs(memberDto, ignoreProperty)))
+                .andExpect(status().isOk());
     }
 }
