998e12aa7251f6e995b122dea2c945354b7917c0
gidskql6671 <gidskql6671@gmail.com>
2021-07-14 21:21:30 +0900

    JPA Auditing 기능을 테스트하는 함수 추가

--- src/test/java/com/study/dong/member/repository/MemberRepositoryTest.java
+++ src/test/java/com/study/dong/member/repository/MemberRepositoryTest.java
@@ -1,43 +1,100 @@
 package com.study.dong.member.repository;
 
 import com.study.dong.member.domain.Member;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityNotFoundException;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
+// Transactional을 붙여주면, 테스트 이후 db가 롤백된다.
+@Transactional
 @SpringBootTest
+@Slf4j
 class MemberRepositoryTest {
 
     @Autowired
     MemberRepository memberRepository;
 
-    @Test
-    @DisplayName("UserVO 생성 테스트")
-    public void testCreateUserVO(){
-        Member member = Member.builder()
+    @BeforeEach
+    public void clearDB(){
+        memberRepository.deleteAll();
+    }
+    
+    private void createMember(){
+        memberRepository.saveAndFlush(Member.builder()
                 .userId("testId")
                 .userPassword("testPassword")
                 .email("testEmail")
                 .name("testName")
                 .nickname("testNickname")
-                .build();
+                .build()
+        );
+    }
+    
+    @Test
+    public void CreateMemberTest(){
+        // given
+        LocalDateTime now = LocalDateTime.of(2000, 1, 1, 0, 0, 0);
+        this.createMember();
 
-        memberRepository.save(member);
-
+        // when
+        Member member = memberRepository.findByUserId("testId")
+                .orElseThrow(EntityNotFoundException::new);
+        
+        // then
         assertAll(() -> assertEquals("testId", member.getUserId()),
                 () -> assertEquals("testPassword", member.getUserPassword()),
                 () -> assertEquals("testEmail", member.getEmail()),
                 () -> assertEquals("testName", member.getName()),
-                () -> assertEquals("testNickname", member.getNickname()),
-                () -> assertEquals(LocalDate.now().toString(), member.getRegDate().toString())
+                () -> assertEquals("testNickname", member.getNickname())
                 );
+        assertThat(member.getCreateDate()).isAfter(now);
+        assertThat(member.getModifiedDate()).isAfter(now);
+        
     }
+    
+    @Test
+    public void updateMemberTest() throws InterruptedException {
+        // given
+        final String email = "testemail2";
+        final String name = "testname2";
+        final String nickname = "testnickname2";
+        
+        this.createMember();
+        
+        Thread.sleep(1000);
+        
+        Member member = memberRepository.findByUserId("testId")
+                .orElseThrow(EntityNotFoundException::new);
+        member.updateInfo(email, name, nickname);
+        memberRepository.saveAndFlush(member);
+        
+        // when
+        Member modifiedMember = memberRepository.findByUserId("testId")
+                .orElseThrow(EntityNotFoundException::new);
+        
+        // then
+        assertAll(
+                () -> assertThat(modifiedMember.getName()).isEqualTo(name),
+                () -> assertThat(modifiedMember.getNickname()).isEqualTo(nickname),
+                () -> assertThat(modifiedMember.getEmail()).isEqualTo(email),
+                () -> assertThat(modifiedMember.getModifiedDate()).isAfter(modifiedMember.getCreateDate())
+        );
+        
+        log.info("CreatedDate  : {}", modifiedMember.getCreateDate());
+        log.info("ModifiedDate : {}", modifiedMember.getModifiedDate());
+    }
+    
 }
