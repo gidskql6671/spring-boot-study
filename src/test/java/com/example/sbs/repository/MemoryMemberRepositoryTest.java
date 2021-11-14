70cbaaf37dc6cf4d6974264a7445a91e24ef99b3
gidskql6671 <gidskql6671@gmail.com>
2021-11-15 00:03:57 +0900

    테스팅 코드 추가

--- src/test/java/com/example/sbs/repository/MemoryMemberRepositoryTest.java
+++ src/test/java/com/example/sbs/repository/MemoryMemberRepositoryTest.java
@@ -1,4 +1,75 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.example.sbs.repository;
+
+import com.example.sbs.domain.Member;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 class MemoryMemberRepositoryTest {
-  
+
+    MemoryMemberRepository repository = new MemoryMemberRepository();
+    
+    @AfterEach
+    public void clear(){
+        repository.clear();
+    }
+    
+    @Test
+    public void save() {
+        Member member = new Member();
+        member.setName("test");
+        repository.save(member);
+        
+        Member result = repository.findById(member.getId()).get();
+        assertThat(result).isEqualTo(member);
+    }
+
+    @Test
+    void findById() {
+        Member member1 = new Member();
+        member1.setName("test");
+        repository.save(member1);
+        
+        Member member2 = new Member();
+        member2.setName("test");
+        repository.save(member2);
+        
+        Member result = repository.findById(member1.getId()).get();
+        
+        assertThat(result).isEqualTo(member1);
+        assertThat(result).isNotEqualTo(member2);
+    }
+
+    @Test
+    void findByName() {
+        Member member1 = new Member();
+        member1.setName("test");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("test");
+        repository.save(member2);
+
+        Member result = repository.findByName(member1.getName()).get();
+
+        assertThat(result).isEqualTo(member1);
+        assertThat(result).isNotEqualTo(member2);
+    }
+
+    @Test
+    void findAll() {
+        Member member1 = new Member();
+        member1.setName("test");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("test");
+        repository.save(member2);
+
+        List<Member> result = repository.findAll();
+        assertThat(result.size()).isEqualTo(2);
+    }
 }
