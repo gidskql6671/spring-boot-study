220a283d4bdaf9befd297080dd72387d3b855550
gidskql6671 <gidskql6671@gmail.com>
2022-02-01 22:25:18 +0900

    통합 테스트 Property 설정

--- src/test/java/com/example/member/SbsApplicationTests.java
+++ src/test/java/com/example/member/SbsApplicationTests.java
@@ -3,11 +3,13 @@ package com.example.member;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = "spring.config.location=" +
+		"classpath:/application.yaml" +
+		",classpath:/credential.yaml")
 class SbsApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
