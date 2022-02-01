7c66b94fdcaf19ae94fc37ce866d16ec48151246
gidskql6671 <gidskql6671@gmail.com>
2022-02-01 21:50:39 +0900

    민감정보를 config 파일에 작성하고, 앱에서 가져옴

--- src/main/java/com/example/SbsApplication.java
+++ src/main/java/com/example/SbsApplication.java
@@ -1,13 +1,18 @@
 package com.example;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class SbsApplication {
-
+    private static final String APPLICATION= 
+            "spring.config.location="+
+                    "classpath:/application.yaml,"+
+                    "classpath:/credential.yaml";
     public static void main(String[] args) {
-        SpringApplication.run(SbsApplication.class, args);
+        new SpringApplicationBuilder(SbsApplication.class).properties(APPLICATION).run(args);
+        
+//        SpringApplication.run(SbsApplication.class, args);
     }
 
 }
