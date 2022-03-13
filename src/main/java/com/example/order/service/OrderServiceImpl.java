af9b5d386a827ca0584b016a3c00f0d0a8cab74f
gidskql6671 <gidskql6671@gmail.com>
2022-03-13 15:01:47 +0900

    빈 라이프사이클 콜백을 설정하는 InitializingBean, DisposableBean
    스프링 전용 인터페이스이기에, 스프링에 강하게 의존한다.
    또한, 인터페이스를 구현하기에 초기화, 소멸 메서드의 이름을 변경할 수 없다.
    마지막으로 코드를 수정할 수 없는 외부라이브러리에는 적용할 수 없다.
    인터페이스를 사용하는 방법은 스프링 초창기에 사용한 방법이고, 최근에는 거의 사용하지 않는다.

--- src/main/java/com/example/order/service/OrderServiceImpl.java
+++ src/main/java/com/example/order/service/OrderServiceImpl.java
@@ -5,10 +5,12 @@ import com.example.discount.DiscountPolicy;
 import com.example.member.domain.Member;
 import com.example.member.repository.MemberRepository;
 import com.example.order.domain.Order;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.stereotype.Service;
 
 @Service
-public class OrderServiceImpl implements OrderService {
+public class OrderServiceImpl implements OrderService, InitializingBean, DisposableBean {
 	
 	private final MemberRepository memberRepository;
 	private final DiscountPolicy discountPolicy;
@@ -25,4 +27,15 @@ public class OrderServiceImpl implements OrderService {
 		
 		return new Order(memberId, itemName, itemPrice, discountPrice);
 	}
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		System.out.println("OrderServiceImpl.afterPropertiesSet");
+	}
+	
+	@Override
+	public void destroy() throws Exception {
+		System.out.println("OrderServiceImpl.destroy");
+	}
+
 }
