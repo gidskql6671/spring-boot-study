220122672b2cefc1e395e7601488c7db34c69b2c
gidskql6671 <gidskql6671@gmail.com>
2023-01-22 10:30:46 +0900

    코틀린 프로젝트 생성

---
+++ kotlin/src/main/kotlin/com/example/sbs/SbsApplication.kt
@@ -0,0 +1,11 @@
+package com.example.sbs
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SbsApplication
+
+fun main(args: Array<String>) {
+    runApplication<SbsApplication>(*args)
+}
