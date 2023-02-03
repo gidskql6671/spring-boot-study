029963cf07b929bfd0da2b482cdf21eaa957dc1c
gidskql6671 <gidskql6671@gmail.com>
2023-02-03 22:38:17 +0900

    request scope Bean 사용 - Proxy 사용하여 주입
    Proxy는 객체 조회를 필요한 시점까지 지연처리한다는 점이 중요함
    꼭 웹 스코프가 아니어도 프록시는 사용가능하다. (프로토타입 스코프에도 적용가능)

--- spring_core_principle_basic/src/main/kotlin/hello/core/web/controller/LogDemoController.kt
+++ spring_core_principle_basic/src/main/kotlin/hello/core/web/controller/LogDemoController.kt
@@ -3,19 +3,17 @@ package hello.core.web.controller
 import hello.core.common.MyLogger
 import hello.core.web.service.LogDemoService
 import jakarta.servlet.http.HttpServletRequest
-import org.springframework.beans.factory.ObjectProvider
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class LogDemoController(
     private val logDemoService: LogDemoService,
-    private val myLoggerProvider: ObjectProvider<MyLogger>
+    private val myLogger: MyLogger
 ) {
 
     @GetMapping("log-demo")
     fun logDemo(request: HttpServletRequest): String {
-        val myLogger = myLoggerProvider.getObject()
         myLogger.requestURL = request.requestURL.toString()
 
         myLogger.log("controller test")
--- spring_core_principle_basic/src/main/kotlin/hello/core/web/service/LogDemoService.kt
+++ spring_core_principle_basic/src/main/kotlin/hello/core/web/service/LogDemoService.kt
@@ -1,15 +1,12 @@
 package hello.core.web.service
 
 import hello.core.common.MyLogger
-import org.springframework.beans.factory.ObjectProvider
 import org.springframework.stereotype.Service
 
 @Service
-class LogDemoService(private val myLoggerProvider: ObjectProvider<MyLogger>) {
+class LogDemoService(private val myLogger: MyLogger) {
 
     fun logic(id: String) {
-        val myLogger = myLoggerProvider.getObject()
-
         myLogger.log("service id = $id")
     }
 }
