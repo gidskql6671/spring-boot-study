371bb8211ca13f7e1ed80ecfbacea6795307d3d3
gidskql6671 <gidskql6671@gmail.com>
2023-02-03 22:55:40 +0900

    스프링 부트와 Jpa활용 1 프젝 생성

---
+++ spring_boot_jpa_utilize_1/src/main/kotlin/jpabook/jpashop/SpringBootJpaUtilize1Application.kt
@@ -0,0 +1,11 @@
+package jpabook.jpashop
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootJpaUtilize1Application
+
+fun main(args: Array<String>) {
+    runApplication<SpringBootJpaUtilize1Application>(*args)
+}
