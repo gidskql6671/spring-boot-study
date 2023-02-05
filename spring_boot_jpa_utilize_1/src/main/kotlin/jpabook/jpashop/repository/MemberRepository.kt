64deb84fdbe8ed6b89af2b092fb7f779a0cb16f0
gidskql6671 <gidskql6671@gmail.com>
2023-02-05 20:11:33 +0900

    MemberRepository 구현

--- spring_boot_jpa_utilize_1/src/main/kotlin/jpabook/jpashop/repository/MemberRepository.kt
+++ spring_boot_jpa_utilize_1/src/main/kotlin/jpabook/jpashop/repository/MemberRepository.kt
@@ -7,15 +7,20 @@ import org.springframework.stereotype.Repository
 
 @Repository
 class MemberRepository(
-    @PersistenceContext
-    private val em: EntityManager
+    @PersistenceContext private val em: EntityManager
 ) {
 
-    fun save(member: Member): Long {
+    fun save(member: Member) {
         em.persist(member)
-
-        return member.id!!
     }
 
-    fun find(id: Long): Member = em.find(Member::class.java, id)
+    fun findOne(id: Long): Member = em.find(Member::class.java, id)
+
+    fun findAll(): List<Member> = em.createQuery("select m from Member m", Member::class.java).resultList
+
+    fun findByName(name: String): List<Member> {
+        return em.createQuery("select m from Member m where m.name = :name", Member::class.java)
+            .setParameter("name", name)
+            .resultList
+    }
 }
