96ebf31cb992694f0bd4ac259c07505adc72393a
gidskql6671 <gidskql6671@gmail.com>
2023-02-07 18:50:02 +0900

    주문 레포지토리 개발

---
+++ spring_boot_jpa_utilize_1/src/main/kotlin/jpabook/jpashop/repository/OrderRepository.kt
@@ -0,0 +1,17 @@
+package jpabook.jpashop.repository
+
+import jakarta.persistence.EntityManager
+import jakarta.persistence.PersistenceContext
+import jpabook.jpashop.domain.Order
+import org.springframework.stereotype.Repository
+
+@Repository
+class OrderRepository(
+    @PersistenceContext private val em: EntityManager
+) {
+    fun save(order: Order) {
+        em.persist(order)
+    }
+
+    fun findOne(id: Long): Order = em.find(Order::class.java, id)
+}
