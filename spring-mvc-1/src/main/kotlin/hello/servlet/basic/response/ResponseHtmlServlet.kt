5112aa84f5fe0c043ae44ecd3678716b60a7c838
gidskql6671 <gidskql6671@gmail.com>
2023-02-13 17:56:56 +0900

    Http 데이터 응답

---
+++ spring-mvc-1/src/main/kotlin/hello/servlet/basic/response/ResponseHtmlServlet.kt
@@ -0,0 +1,23 @@
+package hello.servlet.basic.response
+
+import javax.servlet.annotation.WebServlet
+import javax.servlet.http.HttpServlet
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+
+@WebServlet(name = "responseHtmlServlet", urlPatterns = ["/response-html"])
+class ResponseHtmlServlet: HttpServlet() {
+
+    override fun service(request: HttpServletRequest, response: HttpServletResponse) {
+        response.contentType = "text/html"
+        response.characterEncoding = "utf-8"
+
+        val writer = response.writer
+        writer.println("<html>")
+        writer.println("<body>")
+        writer.println(" <div>안녕?</div>")
+        writer.println("</body>")
+        writer.println("</html>")
+    }
+}
