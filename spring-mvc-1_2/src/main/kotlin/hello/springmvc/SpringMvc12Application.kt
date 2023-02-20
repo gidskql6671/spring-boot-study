48e0ac5e03b49264f8079f5eac9f530d11cc7172
gidskql6671 <gidskql6671@gmail.com>
2023-02-20 20:16:11 +0900

    spring mvc 1의 두번째 프젝을 만듬

---
+++ spring-mvc-1_2/src/main/kotlin/hello/springmvc/SpringMvc12Application.kt
@@ -0,0 +1,11 @@
+package hello.springmvc
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringMvc12Application
+
+fun main(args: Array<String>) {
+    runApplication<SpringMvc12Application>(*args)
+}
