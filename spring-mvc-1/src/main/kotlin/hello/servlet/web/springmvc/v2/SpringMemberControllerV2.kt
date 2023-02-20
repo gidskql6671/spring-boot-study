3867740e64c1379b4f63f481910d0bec994f288f
gidskql6671 <gidskql6671@gmail.com>
2023-02-20 20:06:12 +0900

    spring mvc 기본 2

---
+++ spring-mvc-1/src/main/kotlin/hello/servlet/web/springmvc/v2/SpringMemberControllerV2.kt
@@ -0,0 +1,42 @@
+package hello.servlet.web.springmvc.v2
+
+import hello.servlet.domain.member.Member
+import hello.servlet.domain.member.MemberRepository
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.servlet.ModelAndView
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+@Controller
+@RequestMapping("/springmvc/v2/members")
+class SpringMemberControllerV2 {
+    private val memberRepository = MemberRepository.getInstance()
+
+    @RequestMapping("/new-form")
+    fun newForm(): ModelAndView {
+        return ModelAndView("new-form")
+    }
+
+    @RequestMapping
+    fun members(): ModelAndView {
+        val members: List<Member> = memberRepository.findAll()
+
+        val mv = ModelAndView("members")
+        mv.addObject("members", members)
+        return mv
+    }
+
+    @RequestMapping("/save")
+    fun save(request: HttpServletRequest, response: HttpServletResponse): ModelAndView {
+        val username = request.getParameter("username")
+        val age = request.getParameter("age").toInt()
+
+        val member = Member(username, age)
+        memberRepository.save(member)
+
+        val mv = ModelAndView("save-result")
+        mv.addObject("member", member)
+        return mv
+    }
+}
