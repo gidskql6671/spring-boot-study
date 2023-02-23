5519a81e2ecd1c707d549847e633406ac672eed5
gidskql6671 <gidskql6671@gmail.com>
2023-02-23 13:20:34 +0900

    spring mvc 1의 세번째 프젝을 만듬

---
+++ spring-mvc-1_3/src/main/kotlin/hello/itemservice/SpringMvc13Application.kt
@@ -0,0 +1,11 @@
+package hello.itemservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringMvc13Application
+
+fun main(args: Array<String>) {
+    runApplication<SpringMvc13Application>(*args)
+}
